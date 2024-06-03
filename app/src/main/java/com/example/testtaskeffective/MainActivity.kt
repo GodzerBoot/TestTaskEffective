11e2063349dc4a047bb8615ee8484e578bf72ea8
Ivan Chernenko <reservaccivan@gmail.com>
2024-06-03 23:51:38 +0300

    It renders! Yay

--- app/src/main/java/com/example/testtaskeffective/MainActivity.kt
+++ app/src/main/java/com/example/testtaskeffective/MainActivity.kt
@@ -3,14 +3,13 @@ package com.example.testtaskeffective
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.testtaskeffective.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
 
-        setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/testtaskeffective/fragments/AviaticketsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.testtaskeffective.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.testtaskeffective.R
+import com.example.testtaskeffective.databinding.FragmentAviaticketsBinding
+
+class AviaticketsFragment : Fragment(R.layout.fragment_aviatickets){
+
+}
